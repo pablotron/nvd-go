ed0ecbc492b862e708aa89ed336e13b68166b50f
Paul Duncan <pabs@pablotron.org>
2023-07-14 17:20:33 -0400

    cpe/cpe.go: document errBadPrefix

--- cpe/cpe.go
+++ cpe/cpe.go
@@ -9,6 +9,8 @@ import (
 // Expected prefix components of CPE names and matches.
 var expPrefix = []string { "cpe", "2.3" }
 
+// Error returned by checkPrefix() when the component count of the given
+// slice of strings is too small.
 var errBadPrefix = errors.New("invalid CPE prefix")
 
 // Check for valid prefix.  
