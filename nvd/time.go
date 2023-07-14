d7461d8f75c414326520f087c21f1f8de80188b4
Paul Duncan <pabs@pablotron.org>
2023-07-14 10:47:23 -0400

    nvd/time.go: add Time(), improve documentation

--- nvd/time.go
+++ nvd/time.go
@@ -1,11 +1,14 @@
 package nvd
 
 import (
+  "errors"
   "time"
 )
 
+// Time instance.
 type Time time.Time
 
+// Parse given string as Time or panic on error.
 func MustParseTime(s string) *Time {
   if t, err := NewTime(s); err != nil {
     panic(err)
@@ -14,6 +17,8 @@ func MustParseTime(s string) *Time {
   }
 }
 
+// Create new Time object from given string.  Returns error if the given
+// string could not be parsed as a time.
 func NewTime(s string) (*Time, error) {
   if t, err := time.Parse(time.RFC3339, s); err == nil {
     r := Time(t)
@@ -23,6 +28,22 @@ func NewTime(s string) (*Time, error) {
   }
 }
 
+// Error returned by `Time()` when the given time value is nil.
+var errNilTime = errors.New("nil time")
+
+// Return time as a time.Time object.  Returns an error if the given
+// time value is nil.
+func (t *Time) Time() (*time.Time, error) {
+  if t != nil {
+    r := time.Time(*t)
+    return &r, nil
+  } else {
+    return nil, errNilTime
+  }
+}
+
+// Return time as a RFC3339-formatted string.  Returns an empty string
+// if the given time value is nil.
 func (t *Time) String() string {
   if t != nil {
     return time.Time(*t).Format(time.RFC3339)
