37c31fd4593b42389bd20482157e9dbebf44d51d
Paul Duncan <pabs@pablotron.org>
2023-07-17 02:58:26 -0400

    nvd-api/: use rfc3339.DateTime instead of rfc3339.Time for several fields

--- nvd-api/response.go
+++ nvd-api/response.go
@@ -2,13 +2,13 @@ package nvd_api
 
 import "pmdn.org/nvd-go/rfc3339"
 
-// API response
+// NVD API response.
 type Response struct {
   ResultsPerPage int `json:"resultsPerPage"`
   StartIndex int `json:"startIndex"`
   TotalResults int `json:"totalResults"`
   Format string `json:"format"`
   Version string `json:"version"`
-  Timestamp *rfc3339.Time `json:"timestamp"`
+  Timestamp *rfc3339.DateTime `json:"timestamp"`
   Vulnerabilities *[]Vulnerability `json:"vulnerabilities"`
 }
