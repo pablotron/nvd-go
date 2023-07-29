4f1299f304b89d324f5b2b4a85722d57a1b26809
Paul Duncan <pabs@pablotron.org>
2023-07-29 02:36:38 -0400

    nvd-api/client.go: add UserAgent and HttpClient properties, add send()

--- nvd-api/client.go
+++ nvd-api/client.go
@@ -1,12 +1,25 @@
 package nvd_api
 
 import (
+  "encoding/json"
   "fmt"
+  // "io"
+  "log"
+  "net/http"
   net_url "net/url"
 )
 
+// shared http client
+var httpClient = &http.Client{}
+
 // NVD API client.
 type Client struct {
+  // User agent
+  UserAgent string
+
+  // HTTP client
+  HttpClient *http.Client
+
   // API key
   apiKey string
 
@@ -16,7 +29,7 @@ type Client struct {
 
 // Create new REST API client from given API key and server URL.
 func NewClientWithUrl(apiKey string, apiUrl *net_url.URL) Client {
-  return Client { apiKey: apiKey, apiUrl: apiUrl }
+  return Client { apiKey: apiKey, apiUrl: apiUrl, HttpClient: httpClient }
 }
 
 // Create new REST API client from given API key and default API URL.
@@ -24,6 +37,70 @@ func NewClient(apiKey string) Client {
   return NewClientWithUrl(apiKey, &DefaultUrl)
 }
 
+// Send API request.
+func (c Client) send(endpoint, query string) (*Response, error) {
+  // build full URL path
+  path, err := net_url.JoinPath(c.apiUrl.Path, endpoint)
+  if err != nil {
+    return nil, fmt.Errorf("JoinPath(): %w", err)
+  }
+
+  // build request url
+  url := *c.apiUrl
+  url.Path = path
+  url.RawQuery = query
+  log.Printf("url = %s", url.String())
+
+  // create request
+  req, err := http.NewRequest("GET", url.String(), nil)
+  if err != nil {
+    return nil, fmt.Errorf("NewRequest(): %w", err)
+  }
+
+  // add request headers
+  req.Header.Add("Accept", "application/json")
+  req.Header.Add("apiKey", c.apiKey)
+  if c.UserAgent != "" {
+    req.Header.Add("User-Agent", c.UserAgent)
+  }
+
+  // send request, get response
+  resp, err := c.HttpClient.Do(req)
+  if err != nil {
+    return nil, fmt.Errorf("Do(): %w", err)
+  }
+  defer resp.Body.Close()
+
+  // read body
+  // body, err := io.ReadAll(resp.Body)
+  // if err != nil {
+  //   return nil, fmt.Errorf("ReadFull(): %w", err)
+  // }
+  // log.Printf("url = %s", url.String())
+  // log.Printf("body = %s", string(body))
+
+  // read body, decode response
+  var r Response
+  if err := json.NewDecoder(resp.Body).Decode(&r); err != nil {
+    return nil, fmt.Errorf("Decode(): %w", err)
+  }
+
+  // return response
+  return &r, nil
+}
+
 func (c Client) Cves(params CveParams) (*Response, error) {
-  return nil, fmt.Errorf("TODO")
+  // build query string from parameters
+  queryString, err := params.QueryString()
+  if err != nil {
+    return nil, fmt.Errorf("QueryString(): %w", err)
+  }
+
+  // send request, get response
+  r, err := c.send("cves/2.0", queryString)
+
+  // TODO: check response
+
+  // return response
+  return r, nil
 }
