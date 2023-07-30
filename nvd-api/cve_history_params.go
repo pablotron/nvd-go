584faf1667cc34af5f587c156badac537e7a383b
Paul Duncan <pabs@pablotron.org>
2023-07-30 08:02:27 -0400

    add nvd-api/cve_history_params{,_test}.go

---
+++ nvd-api/cve_history_params.go
@@ -0,0 +1,51 @@
+package nvd_api
+
+import (
+  "fmt"
+  "pmdn.org/nvd-go/cve"
+  "pmdn.org/nvd-go/rfc3339"
+  "pmdn.org/nvd-go/url-params"
+)
+
+// Search parameters for `CveHistory()` method.
+type CveHistoryParams struct {
+  CveId *cve.Id `url:"cveId"`
+  ChangeStartDate *rfc3339.Time `url:"changeStartDate"`
+  ChangeEndDate *rfc3339.Time `url:"changeEndDate"`
+  EventName cve.EventName `url:"eventName"`
+  ResultsPerPage uint `url:"resultsPerPage"`
+  StartIndex uint `url:"startIndex"`
+}
+
+// maximum value for CveHistoryParams ResultsPerPage parameter.
+const cveHistoryParamsMaxResultsPerPage = 5000
+
+// Check CVE parameters for validity.
+func (p CveHistoryParams) Check() error {
+  // check results per page
+  if p.ResultsPerPage > cveHistoryParamsMaxResultsPerPage {
+    return fmt.Errorf("results per page out of bounds: %d > %d", p.ResultsPerPage, cveHistoryParamsMaxResultsPerPage)
+  }
+
+  // check lastMod date range validity
+  if err := checkDateRange("change", p.ChangeStartDate, p.ChangeEndDate); err != nil {
+    return err
+  }
+
+  // return success
+  return nil
+}
+
+// Get parameters encoded as URL query string.
+//
+// Returns an error if any of the parameters are invalid or if an
+// invalid combination of parameters was provided.
+func (p *CveHistoryParams) QueryString() (string, error) {
+  // check for invalid parameter combinations
+  if err := p.Check(); err != nil {
+    return "", err
+  }
+
+  // encode parameters as URL query string
+  return url_params.Encode(p)
+}
---
+++ nvd-api/cve_history_params_test.go
@@ -0,0 +1,60 @@
+package nvd_api
+
+import (
+  "pmdn.org/nvd-go/cve"
+  "pmdn.org/nvd-go/rfc3339"
+  "testing"
+)
+
+func TestCveHistoryParamsQueryString(t *testing.T) {
+  passTests := []struct {
+    name  string // test name
+    val   CveHistoryParams // test value
+    exp   string // expected query string
+  } {{
+    name: "blank",
+    val: CveHistoryParams {},
+    exp: "",
+  }, {
+    name: "cveId",
+    val: CveHistoryParams {
+      CveId: cve.MustParseId("CVE-2023-1234"),
+    },
+    exp: "cveId=CVE-2023-1234",
+  }, {
+    name: "changeEndDate and changeStartDate",
+    val: CveHistoryParams {
+      ChangeEndDate: rfc3339.MustParseTime("2023-12-01T12:34:56Z"),
+      ChangeStartDate: rfc3339.MustParseTime("2023-12-01T12:34:56Z"),
+    },
+    exp: "changeEndDate=2023-12-01T12%3A34%3A56Z&changeStartDate=2023-12-01T12%3A34%3A56Z",
+  }, {
+    name: "resultsPerPage",
+    val: CveHistoryParams {
+      ResultsPerPage: 1999,
+    },
+    exp: "resultsPerPage=1999",
+  }, {
+    name: "startIndex",
+    val: CveHistoryParams {
+      StartIndex: 31415,
+    },
+    exp: "startIndex=31415",
+  }}
+
+  // run pass tests
+  for _, test := range(passTests) {
+    t.Run(test.name, func(t *testing.T) {
+      // get query string
+      got, err := test.val.QueryString()
+      if err != nil {
+        t.Fatal(err)
+      }
+
+      // check for expected value
+      if got != test.exp {
+        t.Fatalf("got %s, exp %s", got, test.exp)
+      }
+    })
+  }
+}
