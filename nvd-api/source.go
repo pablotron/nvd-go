cb2f128c151425ae63eba7176b00ecbe66bb14fa
Paul Duncan <pabs@pablotron.org>
2023-07-31 12:43:12 -0400

    nvd-api/source.go: add missing omitempty annotations

--- nvd-api/source.go
+++ nvd-api/source.go
@@ -7,12 +7,12 @@ import (
 type Source struct {
   // FIXME: not documented in `source_api_json_2.0.schema`, but present
   // in `responses/sources-20.json.gz`.
-  Name string `json:"name"`
-  ContactEmail string `json:"contactEmail"`
+  Name string `json:"name,omitempty"`
+  ContactEmail string `json:"contactEmail,omitempty"`
   LastModified rfc3339.DateTime `json:"lastModified"`
   Created rfc3339.DateTime `json:"published"`
-  V2AcceptanceLevel *AcceptanceLevel `json:"v2AcceptanceLevel"`
-  V3AcceptanceLevel *AcceptanceLevel `json:"v3AcceptanceLevel"`
-  CweAcceptanceLevel *AcceptanceLevel `json:"cweAcceptanceLevel"`
+  V2AcceptanceLevel *AcceptanceLevel `json:"v2AcceptanceLevel,omitempty"`
+  V3AcceptanceLevel *AcceptanceLevel `json:"v3AcceptanceLevel,omitempty"`
+  CweAcceptanceLevel *AcceptanceLevel `json:"cweAcceptanceLevel,omitempty"`
   SourceIdentifiers []string `json:"sourceIdentifiers"`
 }
