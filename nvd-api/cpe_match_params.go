a5962875a14b0dd3972b2cdce8a4b9de77ff6f3c
Paul Duncan <pabs@pablotron.org>
2023-07-31 09:24:36 -0400

    add nvd-api/cpe_match_params{,_test}.go

---
+++ nvd-api/cpe_match_params.go
@@ -0,0 +1,52 @@
+package nvd_api
+
+import (
+  "fmt"
+  "github.com/google/uuid"
+  "pmdn.org/nvd-go/cve"
+  "pmdn.org/nvd-go/rfc3339"
+  "pmdn.org/nvd-go/url-params"
+)
+
+// Search parameters for `CpeMatches()` method.
+type CpeMatchParams struct {
+  CveId *cve.Id `url:"cveId"`
+  LastModStartDate *rfc3339.Time `url:"lastModStartDate"`
+  LastModEndDate *rfc3339.Time `url:"lastModEndDate"`
+  MatchCriteriaId uuid.UUID `url:"matchCriteriaId"`
+  ResultsPerPage uint `url:"resultsPerPage"`
+  StartIndex uint `url:"startIndex"`
+}
+
+// maximum value for CpeMatchParams ResultsPerPage parameter.
+const cpeMatchParamsMaxResultsPerPage = 5000
+
+// Check CPE match parameters for validity.
+func (p CpeMatchParams) Check() error {
+  // check results per page
+  if p.ResultsPerPage > cpeMatchParamsMaxResultsPerPage {
+    return fmt.Errorf("results per page out of bounds: %d > %d", p.ResultsPerPage, cpeMatchParamsMaxResultsPerPage)
+  }
+
+  // check lastMod date range validity
+  if err := checkDateRange("lastMod", p.LastModStartDate, p.LastModEndDate); err != nil {
+    return err
+  }
+
+  // return success
+  return nil
+}
+
+// Get CPE match parameters encoded as URL query string.
+//
+// Returns an error if any of the search parameters are invalid or if an
+// invalid combination of search parameters was provided.
+func (p *CpeMatchParams) QueryString() (string, error) {
+  // check for invalid parameter combinations
+  if err := p.Check(); err != nil {
+    return "", err
+  }
+
+  // encode parameters as URL query string
+  return url_params.Encode(p)
+}
---
+++ nvd-api/cpe_match_params_test.go
@@ -0,0 +1,148 @@
+package nvd_api
+
+import (
+  "github.com/google/uuid"
+  "pmdn.org/nvd-go/cve"
+  "pmdn.org/nvd-go/rfc3339"
+  "testing"
+)
+
+func TestCpeMatchParamsCheck(t *testing.T) {
+  passTests := []struct {
+    name  string // test name
+    val   CpeMatchParams // test value
+  } {{
+    name: "blank",
+    val: CpeMatchParams {},
+  }, {
+    name: "cveId",
+    val: CpeMatchParams {
+      CveId: cve.MustParseId("CVE-2020-1234"),
+    },
+  }, {
+    name: "lastModEndDate AND lastModStartDate",
+    val: CpeMatchParams {
+      LastModEndDate: rfc3339.MustParseTime("2023-12-02T12:34:56Z"),
+      LastModStartDate: rfc3339.MustParseTime("2023-12-01T12:34:56Z"),
+    },
+  }, {
+    name: "matchCriteriaId",
+    val: CpeMatchParams {
+      MatchCriteriaId: uuid.MustParse("87316812-5F2C-4286-94FE-CC98B9EAEF53"),
+    },
+  }, {
+    name: "resultsPerPage",
+    val: CpeMatchParams {
+      ResultsPerPage: 1999,
+    },
+  }, {
+    name: "startIndex",
+    val: CpeMatchParams {
+      StartIndex: 31415,
+    },
+  }}
+
+  // run pass tests
+  for _, test := range(passTests) {
+    t.Run(test.name, func(t *testing.T) {
+      // check parameters
+      if err := test.val.Check(); err != nil {
+        t.Fatal(err)
+      }
+    })
+  }
+
+  failTests := []struct {
+    name  string // test name
+    val   CpeMatchParams // test value
+  } {{
+    name: "invalid resultsPerPage",
+    val: CpeMatchParams {
+      ResultsPerPage: 5001,
+    },
+  }, {
+    name: "invalid date range",
+    val: CpeMatchParams {
+      LastModEndDate: rfc3339.MustParseTime("2023-12-01T12:34:56Z"),
+      LastModStartDate: rfc3339.MustParseTime("2023-12-02T12:34:56Z"),
+    },
+  }, {
+    name: "missing start date",
+    val: CpeMatchParams {
+      LastModEndDate: rfc3339.MustParseTime("2023-12-01T12:34:56Z"),
+    },
+  }, {
+    name: "missing start date",
+    val: CpeMatchParams {
+      LastModEndDate: rfc3339.MustParseTime("2023-12-01T12:34:56Z"),
+    },
+  }}
+
+  // run fail tests
+  for _, test := range(failTests) {
+    t.Run(test.name, func(t *testing.T) {
+      if err := test.val.Check(); err == nil {
+        t.Fatalf("got success, exp err")
+      }
+    })
+  }
+}
+
+func TestCpeMatchParamsQueryString(t *testing.T) {
+  passTests := []struct {
+    name  string // test name
+    val   CpeMatchParams // test value
+    exp   string // expected query string
+  } {{
+    name: "blank",
+    val: CpeMatchParams {},
+    exp: "",
+  }, {
+    name: "cveId",
+    val: CpeMatchParams {
+      CveId: cve.MustParseId("CVE-2023-1234"),
+    },
+    exp: "cveId=CVE-2023-1234",
+  }, {
+    name: "lastModEndDate and lastModStartDate",
+    val: CpeMatchParams {
+      LastModEndDate: rfc3339.MustParseTime("2023-12-01T12:34:56Z"),
+      LastModStartDate: rfc3339.MustParseTime("2023-12-01T12:34:56Z"),
+    },
+    exp: "lastModEndDate=2023-12-01T12%3A34%3A56Z&lastModStartDate=2023-12-01T12%3A34%3A56Z",
+  }, {
+    name: "matchCriteriaId",
+    val: CpeMatchParams {
+      MatchCriteriaId: uuid.MustParse("87316812-5F2C-4286-94FE-CC98B9EAEF53"),
+    },
+    exp: "matchCriteriaId=87316812-5f2c-4286-94fe-cc98b9eaef53",
+  }, {
+    name: "resultsPerPage",
+    val: CpeMatchParams {
+      ResultsPerPage: 4999,
+    },
+    exp: "resultsPerPage=4999",
+  }, {
+    name: "startIndex",
+    val: CpeMatchParams {
+      StartIndex: 31415,
+    },
+    exp: "startIndex=31415",
+  }}
+
+  // run pass tests
+  for _, test := range(passTests) {
+    t.Run(test.name, func(t *testing.T) {
+      // get query string
+      got, err := test.val.QueryString()
+      if err != nil {
+        t.Fatal(err)
+      }
+
+      // check for expected value
+      if got != test.exp {
+        t.Fatalf("got %s, exp %s", got, test.exp)
+      }
+    })
+  }
+}
