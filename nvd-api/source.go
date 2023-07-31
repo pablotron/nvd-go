61fa7b49936d12868252f80f95f74319ca98ceb8
Paul Duncan <pabs@pablotron.org>
2023-07-31 12:08:28 -0400

    nvd-api/source.go: add undocumented Name field

--- nvd-api/source.go
+++ nvd-api/source.go
@@ -5,6 +5,9 @@ import (
 )
 // NVD API data source.
 type Source struct {
+  // FIXME: not documented in `source_api_json_2.0.schema`, but present
+  // in `responses/sources-20.json.gz`.
+  Name string `json:"name"`
   ContactEmail string `json:"contactEmail"`
   LastModified rfc3339.DateTime `json:"lastModified"`
   Created rfc3339.DateTime `json:"published"`
