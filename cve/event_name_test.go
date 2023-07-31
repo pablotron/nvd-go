f8d9a50bcfbd45a1ce5df8f99a9f3bd08766c186
Paul Duncan <pabs@pablotron.org>
2023-07-31 15:43:22 -0400

    cve/event_name_test.go: add TestEventNameUnmarshalText()

--- cve/event_name_test.go
+++ cve/event_name_test.go
@@ -4,6 +4,56 @@ import (
   "testing"
 )
 
+func TestEventNameUnmarshalText(t *testing.T) {
+  passTests := []struct {
+    name string // test name
+    val string // test value
+    exp EventName // expected string
+  } {
+    { "InitialAnalysis", "Initial Analysis", InitialAnalysis },
+    { "Reanalysis", "Reanalysis", Reanalysis },
+    { "CveModified", "CVE Modified", CveModified },
+    { "ModifiedAnalysis", "Modified Analysis", ModifiedAnalysis },
+    { "CveTranslated", "CVE Translated", CveTranslated },
+    { "VendorComment", "Vendor Comment", VendorComment },
+    { "CveSourceUpdate", "CVE Source Update", CveSourceUpdate },
+    { "CpeDeprecationRemap", "CPE Deprecation Remap", CpeDeprecationRemap },
+    { "CweRemap", "CWE Remap", CweRemap },
+    { "CveRejected", "CVE Rejected", CveRejected },
+    { "CveUnrejected", "CVE Unrejected", CveUnrejected },
+  }
+
+  for _, test := range(passTests) {
+    t.Run(test.name, func(t *testing.T) {
+      var got EventName
+      if err := got.UnmarshalText([]byte(test.val)); err != nil {
+        t.Fatal(err)
+      }
+
+      if got != test.exp {
+        t.Fatalf("got \"%s\", exp \"%s\"", got, test.exp)
+      }
+    })
+  }
+
+  failTests := []struct {
+    name string // test name
+    val string // test value
+  } {
+    { "empty", "" },
+    { "invalid", "asdf" },
+  }
+
+  for _, test := range(failTests) {
+    t.Run(test.name, func(t *testing.T) {
+      var got EventName
+      if err := got.UnmarshalText([]byte(test.val)); err == nil {
+        t.Fatalf("got \"%s\", exp err", got)
+      }
+    })
+  }
+}
+
 func TestEventNameString(t *testing.T) {
   tests := []struct {
     name string // test name
