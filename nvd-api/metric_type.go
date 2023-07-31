a7f7cf51210637a4832d2555145a75d015fb1c0e
Paul Duncan <pabs@pablotron.org>
2023-07-31 17:39:43 -0400

    nvd-api/metric_type.go: add metricTypeStrMap and metricTypeStrs

--- nvd-api/metric_type.go
+++ nvd-api/metric_type.go
@@ -3,25 +3,27 @@ package nvd_api
 import "fmt"
 
 // CVSS metric type (primary or secondary).
-type MetricType byte
+type MetricType uint8
 
 const (
   InvalidMetricType MetricType = iota
-  Primary // AND
-  Secondary // OR
+  Primary // Primary
+  Secondary // Secondary
 )
 
-// Unmarshal metric type
-func (t *MetricType) UnmarshalText(text []byte) error {
-  s := string(text)
-  switch string(text) {
-  case "Primary":
-    *t = Primary
+// Map of string to metric type.  Used by `UnmarshalText()`.
+var metricTypeStrMap = map[string]MetricType {
+  "Primary": Primary,
+  "Secondary": Secondary,
+}
+
+// Unmarshal metric type from text.
+func (t *MetricType) UnmarshalText(b []byte) error {
+  s := string(b)
+  if nt, ok := metricTypeStrMap[s]; ok {
+    *t = nt
     return nil
-  case "Secondary":
-    *t = Secondary
-    return nil
-  default:
+  } else {
     return fmt.Errorf("invalid metric type: \"%s\"", s)
   }
 }
@@ -31,14 +33,18 @@ func (t *MetricType) MarshalText() ([]byte, error) {
   return []byte(t.String()), nil
 }
 
+// Map of metric type to string.  Used by `String()`.
+var metricTypeStrs = [...]string {
+  "", // InvalidMetricType
+  "Primary", // Primary
+  "Secondary", // Secondary
+}
+
 // Convert metric type to string.
 func (t MetricType) String() string {
-  switch t {
-  case Primary:
-    return "Primary"
-  case Secondary:
-    return "Secondary"
-  default:
+  if int(t) < len(metricTypeStrs) {
+    return metricTypeStrs[int(t)]
+  } else {
     return ""
   }
 }
