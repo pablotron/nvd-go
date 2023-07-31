5c2ff4097093282c9100ec7b936d4fea7cb762ab
Paul Duncan <pabs@pablotron.org>
2023-07-31 17:09:57 -0400

    nvd-api/query_stringer.go: document QueryString interface

--- nvd-api/query_stringer.go
+++ nvd-api/query_stringer.go
@@ -1,6 +1,8 @@
 package nvd_api
 
+// Interface used by Client.send() to marshal tagged structure as URL
+// query string.
 type QueryStringer interface {
-  // Convert structure to query string.
+  // Convert tagged structure fields to URL query string.
   QueryString() (string, error)
 }
