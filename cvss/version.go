4ce7fe6096a8e65c025edbe01629e76c03e9ce52
Paul Duncan <pabs@pablotron.org>
2023-07-31 16:50:22 -0400

    cvss/version.go: add versionStrs and UnmarshalText()

--- cvss/version.go
+++ cvss/version.go
@@ -7,12 +7,13 @@ type Version byte
 
 const (
   UnknownVersion Version = iota
-  V2
-  V30
-  V31
-  // V4
+  V2 // CVSS 2
+  V30 // CVSS v3.0
+  V31 // CVSS v3.1
+  // TODO: V4
 )
 
+// Version string to version map.
 var versions = map[string]Version {
   "2.0": V2,
   "3.0": V30,
@@ -28,20 +29,36 @@ func ParseVersion(s string) (*Version, error) {
   }
 }
 
+// Unmarshal CVSS version from text.
+func (v *Version) UnmarshalText(b []byte) error {
+  s := string(b)
+  if nv, err := ParseVersion(s); err == nil {
+    *v = *nv
+    return nil
+  } else {
+    return err
+  }
+}
+
+// Version strings.  used by `String()`.
+var versionStrs = [...]string {
+  "",
+  "2.0",
+  "3.0",
+  "3.1",
+}
+
+// Convert CVSS version to string.  Returns "" if the given version
+// string is not valid.
 func (v Version) String() string {
-  switch v {
-  case V2:
-    return "2.0"
-  case V30:
-    return "3.0"
-  case V31:
-    return "3.1"
-  default:
+  if int(v) < len(versionStrs) {
+    return versionStrs[int(v)]
+  } else {
     return ""
   }
 }
 
-// Is this a valid severity for the given CVSS version?
+// Is the given severity valid for this CVSS version?
 func (v Version) ValidSeverity(s Severity) bool {
   switch v {
   case V2:
