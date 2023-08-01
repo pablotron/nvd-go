fe732b76d05c316b1ff2928bf8e256e8baa294ad
Paul Duncan <pabs@pablotron.org>
2023-08-01 01:37:03 -0400

    cvss/score_test.go: add TestMustParseScore(), TestUnmarshalJSON(), TestMarshalText()

--- cvss/score_test.go
+++ cvss/score_test.go
@@ -1,6 +1,7 @@
 package cvss
 
 import (
+  "fmt"
   "strconv"
   "testing"
 )
@@ -27,6 +28,36 @@ func TestParseScore(t *testing.T) {
       if got, err := ParseScore(val); err == nil {
         t.Errorf("got %v, exp error", got)
       }
+    })
+  }
+}
+
+func TestMustParseScore(t *testing.T) {
+  // pass tests
+  for exp := 0; exp < 100; exp++ {
+    val := float64(exp) / 10.0
+
+    t.Run(strconv.FormatInt(int64(exp), 10), func(t *testing.T) {
+      defer func() {
+        if err := recover(); err != nil {
+          t.Fatal(err)
+        }
+      }()
+      _ = MustParseScore(val)
+    })
+  }
+
+  // fail tests
+  failTests := []float64 { -10.0, -0.1, 10.1, 100.0 }
+  for _, val := range(failTests) {
+    t.Run(strconv.FormatFloat(val, 'f', 2, 64), func(t *testing.T) {
+      defer func() {
+        if recover() == nil {
+          t.Fatal("got success, exp error")
+        }
+      }()
+
+      _ = MustParseScore(val)
     })
   }
 }
@@ -86,6 +117,52 @@ func TestScoreFloat(t *testing.T) {
       got := s.Float()
       if got != test.exp {
         t.Errorf("got \"%f\", exp \"%f\"", got, test.exp)
+      }
+    })
+  }
+}
+
+func TestScoreUnmarshalJSON(t *testing.T) {
+  // pass tests
+  for test := 0; test < 100; test++ {
+    t.Run(strconv.FormatInt(int64(test), 10), func(t *testing.T) {
+      b := []byte(fmt.Sprintf("%1.1f", float64(test) / 10.0))
+      var score Score
+      if err := score.UnmarshalJSON(b); err != nil {
+        t.Fatal(err)
+      }
+    })
+  }
+
+  // fail tests
+  failTests := []float64 { -10.0, -0.1, 10.1, 100.0 }
+  for _, test := range(failTests) {
+    t.Run(strconv.FormatFloat(test, 'f', 2, 64), func(t *testing.T) {
+      b := []byte(fmt.Sprintf("%f", test))
+
+      var got Score
+      if got.UnmarshalJSON(b) == nil {
+        t.Fatalf("got %v, exp error", got)
+      }
+    })
+  }
+}
+
+func TestScoreMarshalText(t *testing.T) {
+  // pass tests
+  for test := 0; test < 100; test++ {
+    t.Run(strconv.FormatInt(int64(test), 10), func(t *testing.T) {
+      val := float64(test) / 10.0
+
+      gotBytes, err := MustParseScore(val).MarshalText()
+      if err != nil {
+        t.Fatal(err)
+      }
+
+      exp := fmt.Sprintf("%1.1f", val)
+      got := string(gotBytes)
+      if got != exp {
+        t.Fatalf("got \"%s\", exp \"%s\"", got, exp)
       }
     })
   }
