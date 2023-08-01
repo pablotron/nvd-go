941b70308d239b428ed50b85ed6f2321540b62bb
Paul Duncan <pabs@pablotron.org>
2023-08-01 14:27:15 -0400

    nvd-api/ref_url_test.go: add TestMustParseRefUrl()

--- nvd-api/ref_url_test.go
+++ nvd-api/ref_url_test.go
@@ -36,6 +36,50 @@ func TestParseRefUrl(t *testing.T) {
   }
 }
 
+func TestMustParseRefUrl(t *testing.T) {
+  passTests := []string {
+    "http://example.com/",
+    "https://example.com/",
+    "ftp://example.com/",
+    "ftps://example.com/",
+  }
+
+  for _, test := range(passTests) {
+    t.Run(test, func(t *testing.T) {
+      defer func() {
+        if err := recover(); err != nil {
+          t.Fatal(err)
+        }
+      }()
+
+      // parse string
+      _ = MustParseRefUrl(test)
+    })
+  }
+
+  failTests := []struct {
+    name string // test name
+    val string // test value
+  } {
+    { "empty", "" },
+    { "invalid scheme", "foo://example.com/" },
+    { "invalid host", "http://" },
+  }
+
+  for _, test := range(failTests) {
+    t.Run(test.name, func(t *testing.T) {
+      defer func() {
+        if recover() == nil {
+          t.Fatal("got success, exp error")
+        }
+      }()
+
+      // parse string
+      _ = MustParseRefUrl(test.val)
+    })
+  }
+}
+
 func TestRefUrlString(t *testing.T) {
   passTests := []string {
     "http://example.com/",
