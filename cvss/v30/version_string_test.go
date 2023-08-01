87c975c2eeef61eba40f91dad81d40a10ec3b0af
Paul Duncan <pabs@pablotron.org>
2023-08-01 13:11:07 -0400

    add cvss/v{2,30,31}/version_string_test.go

---
+++ cvss/v30/version_string_test.go
@@ -0,0 +1,43 @@
+package v30
+
+import "testing"
+
+func TestVersionString(t *testing.T) {
+  exp := "3.0"
+
+  t.Run("UnmarshalText", func(t *testing.T) {
+    var got VersionString
+
+    // test valid string
+    if err := got.UnmarshalText([]byte(exp)); err != nil {
+      t.Fatal(err)
+    }
+
+    // test invalid string
+    if err := got.UnmarshalText([]byte("garbage")); err == nil {
+      t.Fatalf("got %v, exp error", got)
+    }
+  })
+
+  t.Run("MarshalText", func(t *testing.T) {
+    var v VersionString
+
+    gotBytes, err := v.MarshalText()
+    if err != nil {
+      t.Fatal(err)
+    }
+
+    got := string(gotBytes)
+    if got != exp {
+      t.Fatalf("got %s, exp %s", got, exp)
+    }
+  })
+
+  t.Run("String", func(t *testing.T) {
+    var v VersionString
+    got := v.String()
+    if got != exp {
+      t.Fatalf("got %s, exp %s", got, exp)
+    }
+  })
+}
