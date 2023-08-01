922e9f0a27bda180cb1a9d2425403215ee3d5990
Paul Duncan <pabs@pablotron.org>
2023-08-01 14:25:23 -0400

    nvd-api/version_string_test.go: add TestVersionStringMarshalText()

--- nvd-api/version_string_test.go
+++ nvd-api/version_string_test.go
@@ -18,6 +18,21 @@ func TestVersionStringUnmarshalText(t *testing.T) {
   })
 }
 
+func TestVersionStringMarshalText(t *testing.T) {
+  var v VersionString
+
+  gotBytes, err := v.MarshalText()
+  if err != nil {
+    t.Fatal(err)
+  }
+
+  got := string(gotBytes)
+  exp := "2.0"
+  if got != exp {
+    t.Fatalf("got \"%s\", exp \"%s\"", got, exp)
+  }
+}
+
 func TestVersionStringString(t *testing.T) {
   var v VersionString
 
