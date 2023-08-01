482859e8f8f47e295902025171aba40bab9ca30d
Paul Duncan <pabs@pablotron.org>
2023-08-01 03:10:04 -0400

    cvss/version_test.go: add TestVersionString() and TestVersionUnmarshalText()

--- cvss/version_test.go
+++ cvss/version_test.go
@@ -46,6 +46,29 @@ func TestParseVersion(t *testing.T) {
   }
 }
 
+func TestVersionString(t *testing.T) {
+  passTests := []struct {
+    name string // test name
+    val Version // test version
+    exp string // expected string
+  } {
+    { "V2", V2, "2.0" },
+    { "V30", V30, "3.0" },
+    { "V31", V31, "3.1" },
+    { "UnknownVersion", UnknownVersion, "" },
+    { "Version(255)", Version(255), "" },
+  }
+
+  for _, test := range(passTests) {
+    t.Run(test.name, func(t *testing.T) {
+      got := test.val.String()
+      if got != test.exp {
+        t.Fatalf("got \"%s\", exp \"%s\"", got, test.exp)
+      }
+    })
+  }
+}
+
 func TestValidSeverity(t *testing.T) {
   tests := []struct {
     version Version
@@ -80,6 +103,36 @@ func TestValidSeverity(t *testing.T) {
       got := test.version.ValidSeverity(test.severity)
       if got != test.exp {
         t.Fatalf("got %t, exp %t", got, test.exp)
+      }
+    })
+  }
+}
+
+func TestVersionUnmarshalText(t *testing.T) {
+  passTests := []string {
+    "2.0",
+    "3.0",
+    "3.1",
+  }
+
+  for _, test := range(passTests) {
+    t.Run(test, func(t *testing.T) {
+      var version Version
+      if err := version.UnmarshalText([]byte(test)); err != nil {
+        t.Fatal(err)
+      }
+    })
+  }
+
+  failTests := []string {
+    "asdf",
+  }
+
+  for _, test := range(failTests) {
+    t.Run(test, func(t *testing.T) {
+      var got Version
+      if got.UnmarshalText([]byte(test)) == nil {
+        t.Fatalf("got %v, exp error", got)
       }
     })
   }
