4856b6ddf3c78cb89f95d2e2d5830c107218877f
Paul Duncan <pabs@pablotron.org>
2023-08-01 21:29:19 -0400

    add mock-server/, refactor

---
+++ mock-server/send_json.go
@@ -0,0 +1,39 @@
+package mock_server
+
+import (
+  "compress/gzip"
+  "fmt"
+  "io"
+  "net/http"
+  "os"
+)
+
+
+// Send compressed JSON file as response.
+func sendJson(w http.ResponseWriter, name string) error {
+  // open source file
+  f, err := os.Open(fmt.Sprintf("testdata/responses/%s", name))
+  if err != nil {
+    http.Error(w, "", http.StatusInternalServerError)
+    return err
+  }
+  defer f.Close()
+
+  // create gzip reader
+  gz, err := gzip.NewReader(f)
+  if err != nil {
+    http.Error(w, "", http.StatusInternalServerError)
+    return err
+  }
+  defer gz.Close()
+
+  // set header, copy content
+  w.Header().Add("Content-Type", "application/json")
+  if _, err := io.Copy(w, gz); err != nil {
+    return err
+  }
+
+  // return success
+  return nil
+}
+
